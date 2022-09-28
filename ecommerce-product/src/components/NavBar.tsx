5090ded9431ea7a7aa247332b3a94c04d5a2818a
Mogakamo <mogaka.amo254@gmail.com>
2022-09-28 17:05:23 +0300

    [styling]: configured tailwind according to the style guide

--- ecommerce-product/src/components/NavBar.tsx
+++ ecommerce-product/src/components/NavBar.tsx
@@ -1,3 +1,6 @@
 export default function NavBar() {
-    return <div className="h-auto p-5 border-b-2 mx-20"></div>
+    return <div className="h-auto p-5 border-b-2 mx-20">
+        {/* Logo */}
+        <img src="" alt="" />
+    </div>
 }
